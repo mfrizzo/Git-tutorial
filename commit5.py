15adcea21cca0352214505de461d8678a1cbbc17
mfrizzo <mfrizzo@ufm.edu>
2022-04-20 22:07:00 -0600

    commit5.py

---
+++ commit5.py
@@ -0,0 +1,11 @@
+print('Empezando')
+
+x = True
+y = False
+print('Resultado AND:',x and y)
+print('Resultado OR:',x or y)
+print('Resultado NOT:', not x)
+z = (x and y) or (y or y) and (x and x)
+print(z)
+
+print('Fin del programa')
